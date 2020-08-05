ce9dcb4e53c3ae68aba31a3af35a304dc42f1e13
Jacksson Andrey <jaab.andrey@gmail.com>
2020-08-05 18:52:10 -0300

    Aplicaçao web finalizada

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -3,31 +3,54 @@ import React from 'react';
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
 import './styles.css';
+import api from '../../services/api';
 
-const TeacherItem = () => {
+export interface Teacher {
+  id: number;
+    avatar: string;
+    bio: string;
+    cost: number;
+    name: string;
+    subject: string;
+    whatsapp: string;
+}
+interface TeacherItemProps {
+  teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+  function createNewConnection() {
+    api.post('connections', {
+      user_id: teacher.id
+    });
+  }
+
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars0.githubusercontent.com/u/31444727?s=460&u=3a8890fd92b03ba748117b8f2ea58fca0d3d3504&v=4" alt="Jacksson Andrey"/>
+        <img src={teacher.avatar} alt={teacher.name}/>
         <div>
-          <strong>Jacksson Andrey</strong>
-          <span>Desenvolvimento Web</span>
+          <strong>{teacher.name}</strong>
+          <span>{teacher.subject}</span>
         </div>
       </header>
-      <p>
-        Apaixonado por tecnologia e repassar conhecimento, ja formou mais de 10.000 alunos. Em suas aulas mostra que Desenvolvimento 
-        Web não é nehhum bicho de sete cabeças.
-      </p>
+
+      <p>{teacher.bio}</p>
 
       <footer>
         <p>
           Preço/hora
-          <strong>R$ 200,00</strong>
+          <strong>R$ {teacher.cost}</strong>
         </p>
-        <button type="button">
+        <a 
+          // eslint-disable-next-line react/jsx-no-target-blank
+          target="_blank"
+          onClick={createNewConnection} 
+          href={`https://wa.me/${teacher.whatsapp}?text=Eu%20tenho%20interesse%20nas%suas%aulas.`}
+        >
           <img src={whatsappIcon} alt="Whatsapp"/>
           Entrar em contato
-        </button>
+        </a>
       </footer>
     </article>
   );
